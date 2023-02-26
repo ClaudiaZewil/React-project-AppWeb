aca3458ddeb64f943a5cc49973601aed9ab4be0a
ClaudiaZewil <49198720+ClaudiaZewil@users.noreply.github.com>
2023-02-26 21:18:05 +0100

    fixes

--- progetto-esame/src/components/SongCard/SongCard.js
+++ progetto-esame/src/components/SongCard/SongCard.js
@@ -62,7 +62,7 @@ function SongCard(props) {
                  }
                  <CardBody className="mb-3 text-center">
                      <CardText tag="h3" className={`${style.songNumber}`}>{songNumber}</CardText>
-                     <CardTitle>{songName}</CardTitle>
+                     <CardTitle><h4>{songName}</h4></CardTitle>
                      <CardSubtitle>{songArtist}</CardSubtitle>
                  </CardBody>
              </Card>
