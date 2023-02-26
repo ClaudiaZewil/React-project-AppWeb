1fa497dfa63decc427f66999886ffb01849a9b47
ClaudiaZewil <49198720+ClaudiaZewil@users.noreply.github.com>
2023-02-26 11:17:38 +0100

    aggiornamenti api musicbrainz
    oh raga il json funziona

--- progetto-esame/src/views/SongDetails/SongDetails.js
+++ progetto-esame/src/views/SongDetails/SongDetails.js
@@ -6,6 +6,7 @@ import songDefaultImage from "../../assets/images/crowd.jpeg";
 import style from "../../components/SongTableTr/SongTableTr.module.css";
 import songLoadingImage from "../../assets/images/loading.png";
 import style2 from "./SongDetails.module.css";
+import songDefaultImageBW from "../../assets/images/RollingStoneLogoSmallBW.png";
 
 function SongDetails() {
     let {number} = useParams();
@@ -22,7 +23,7 @@ function SongDetails() {
         for (let i = 0; i < songArtistData.lenght; i++) {
             if (songArtistData[i] === " ") songArtistData[i] = "%20";
         }
-        fetch(`https://musicbrainz.org/ws/2/artist/?query=artist:${songArtistData}`)
+        fetch(`https://musicbrainz.org/ws/2/artist/?query=artist:${songArtistData}&fmt=json`)
             .then(res => res.json())
             .then(res => {
                 if (isMounted)
@@ -62,20 +63,39 @@ function SongDetails() {
                 <div className="col">
                     <div>
                         <h2>{songCurrent["0"]["artistTitle"]}</h2>
-                        <p>
-                            <strong>Type:</strong> {artistData.artists["0"]["type"]}<br/>
-                            <strong>Area:</strong> {artistData.artists["0"]["area"]["name"]}<br/>
-                            <strong>Begin area:</strong> {artistData.artists["0"]["begin-area"]["name"]}<br/>
-                            <strong>Life:</strong> {artistData.artists["0"]["life-span"]["begin"]} -
-                            <span className="d-inline">
+                        {artistData.length !== 0 ?
+                            <div>
                                 {
-                                    artistData.artists["0"]["life-span"]["ended"] === null ?
-                                        <span> alive</span>
+                                    artistData.error ?
+                                        <p>Error: no aviable information</p>
                                         :
-                                        <span> {artistData.artists["0"]["life-span"]["ended"]}</span>
+                                        <p>
+                                            <strong>Type:</strong> {artistData.artists["0"]["type"]}<br/>
+                                            <strong>Area:</strong> {artistData.artists["0"]["area"]["name"]}<br/>
+                                            <strong>Begin area:</strong> {artistData.artists["0"]["begin-area"]["name"]}<br/>
+                                            <strong>Life:</strong> {artistData.artists["0"]["life-span"]["begin"]} -
+                                            <span className="d-inline">
+                                                {
+                                                    artistData.artists["0"]["life-span"]["ended"] === null ?
+                                                        <span> alive</span>
+                                                        :
+                                                        <span> {artistData.artists["0"]["life-span"]["ended"]}</span>
+                                                }
+                                            </span>
+                                        </p>
+
                                 }
-                            </span>
-                        </p>
+                            </div>
+                            :
+                            <div>
+                                {
+                                    apiError === false ?
+                                        <p>Loading information...</p>
+                                        :
+                                        <p>Server: no aviable information</p>
+                                }
+                            </div>
+                        }
                     </div>
                     <div className={style.navigation}>
                         {number - 1 !== 0 &&
